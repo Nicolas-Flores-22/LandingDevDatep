000bd0cb0d9e57dd53c546e6a9356c3ed45a6036
Kamila Ojeda <kamojeda@Kamilas-MacBook-Pro.local>
2023-08-11 10:52:01 -0500

    wid: 🚧 feat/about-us-page add about-us nav link in header component, add manager photo svg and work in about-us page

--- src/pages/public/AboutUs.tsx
+++ src/pages/public/AboutUs.tsx
@@ -1,1 +1,58 @@
-export const AboutUs = () => {};
+import { useEffect } from "react";
+import useObserver from "../../hooks/useObserver";
+import manager_photo from "../../assets/manager_photo.svg";
+
+export const AboutUs = () => {
+  const { observer, setElements, entries } = useObserver({
+    threshold: 0.75,
+    root: null,
+  });
+
+  useEffect(() => {
+    const sections = document.querySelectorAll(".lazy");
+    const sectionsArray: Element[] = Array.from(sections);
+    setElements(sectionsArray);
+  }, [setElements]);
+
+  useEffect(() => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const lazy = entry.target as HTMLImageElement;
+        lazy.src = lazy.dataset.src!;
+        lazy.classList.add(
+          "translate-y-0",
+          "opacity-100",
+          "transition",
+          "duration-1000",
+          "ease-in-out"
+        );
+        lazy.classList.remove("lazy", "opacity-0");
+        observer.unobserve(lazy);
+      }
+    });
+  }, [entries, observer]);
+  return (
+    <div>
+      <section className="w-full lazy">
+        <div className="flex justify-center w-full lg:h-[80vh] bg-[url('./assets/Union.svg')] bg-cover bg-no-repeat">
+          <div className="max-w-[1440px]">
+            <div className="flex flex-col lg:flex-row mt-8 lg:mt-[65px]">
+              <div className="flex flex-col py-10">
+                <h2 className="font-sans mb-5 text-3xl sm:text-4xl font-bold text-white">
+                  ¿Quienes somos?
+                </h2>
+                <p className="mb-8 text-lg text-gray-200">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+                  repellendus omnis sunt natus doloribus suscipit corrupti
+                  laborum vero eius, est cumque pariatur maiores aut ullam vel
+                  temporibus, quas aliquam accusamus.
+                </p>
+              </div>
+              <img src={manager_photo} alt="manager-photo-section" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
