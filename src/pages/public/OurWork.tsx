5e3834490317b032d07dcaa701866811f8a3a929
Nicolas <nicolas_flores22@outlook.com>
2023-08-14 09:50:14 -0500

    feat: :construction: feat/nuestro-trabajo  page OutWork

--- src/pages/public/OurWork.tsx
+++ src/pages/public/OurWork.tsx
@@ -1,8 +1,17 @@
 const OurWork = () => {
   return (
-    <div>
-      Nuestro trabajo
-    </div>
+    <>
+      <div className=" h-8 "></div>
+      <div className="flex flex-col items-center justify-center gap-y-2 mx-auto text-center w-[95%] max-w-[1100px] mt-8">
+        <h2 className="font-montse mb-5 text-3xl sm:text-4xl font-bold text-[#2B426E]">
+          Nuestro Trayectoria Creativa
+        </h2>
+        <p className="mb-8 font text-2xl text-[#535353]">
+          Explora nuestra selección diversa de proyectos realizados con pasión,
+          innovación y compromiso hacia la excelencia
+        </p>
+      </div>
+    </>
   )
 }
 
