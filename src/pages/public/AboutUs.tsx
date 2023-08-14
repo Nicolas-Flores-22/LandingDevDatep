e77a390404545a3d53c97fa26928adb070cd9379
Kamila Ojeda <kamojeda@Kamilas-MacBook-Pro.local>
2023-08-14 09:28:08 -0500

    wid: 🚧 feat/about-us-page add images to the third section

--- src/pages/public/AboutUs.tsx
+++ src/pages/public/AboutUs.tsx
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 import useObserver from "../../hooks/useObserver";
 import manager_photo from "../../assets/manager_photo.svg";
+import aboutUs_section_video from "../../assets/aboutUs_section_video.svg";
+import aboutUs_section_img1 from "../../assets/aboutUs_section_img1.svg";
+import aboutUs_section_img2 from "../../assets/aboutUs_section_img2.svg";
+import aboutUs_section_img3 from "../../assets/aboutUs_section_img3.svg";
 
 export const AboutUs = () => {
   const { observer, setElements, entries } = useObserver({
@@ -49,6 +53,50 @@ export const AboutUs = () => {
                 </p>
               </div>
               <img src={manager_photo} alt="manager-photo-section" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="w-full">
+        <div className="flex flex-col items-center justify-center gap-y-2 mx-auto text-center w-[95%] max-w-[1100px]">
+          <h2 className="w-full mb-6 font-sans text-3xl font-bold tracking-tight text-[#051C46] sm:text-4xl sm:leading-none">
+            ¿Qué hacemos por ti?
+          </h2>
+          <p className="w-full mb-4 text-base md:text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat
+            nam assumenda commodi sint sunt velit deleniti asperiores, odit
+            numquam. Provident aspernatur sunt minus odit expedita ex unde optio
+            dolores temporibus?
+          </p>
+          <div className="lazy opacity-0">
+            <img src={aboutUs_section_video} alt="aboutUs_section_video" />
+          </div>
+        </div>
+      </section>
+      <section className="w-full lazy opacity-0">
+        <div className="flex w-full h-auto lg:h-[700px] mt-20 bg-[url('./assets/diferencia_background.svg')] bg-cover bg-no-repeat ">
+          <div className="w-full flex flex-col items-center gap-16 justify-center max-w-[1300px] mx-auto">
+            <div className="grid grid-cols-2 gap-5">
+              <div className="flex flex-col">
+                <h2 className="text-3xl font-bold text-center text-[#051C46] opacity-80">
+                  Nuestros profesionales son los más capacitados
+                </h2>
+                <p className="text-xl sm:text-2xl font-normal text-center text-[#051C46] opacity-80">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Expedita doloremque, libero aliquid excepturi tenetur
+                  repellendus a omnis error vero eveniet id consectetur quisquam
+                  delectus veniam temporibus voluptatibus obcaecati aliquam
+                  sunt!
+                </p>
+                <h3>Diseñadores</h3>
+                <h3>Desarrolladores</h3>
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                <img src={aboutUs_section_img1} alt="aboutUs_section_img1" />
+                <img src={aboutUs_section_img2} alt="aboutUs_section_img2" />
+                <img src={aboutUs_section_img3} alt="aboutUs_section_img3" />
+                <img src={aboutUs_section_img1} alt="aboutUs_section_img1" />
+              </div>
             </div>
           </div>
         </div>
